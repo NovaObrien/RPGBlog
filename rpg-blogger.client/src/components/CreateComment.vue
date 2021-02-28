39f5b941feacda9b1a241b0a13c3888079dda3e3
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-02-28 02:33:45 -0700

    Updates

--- rpg-blogger.client/src/components/CreateComment.vue
+++ rpg-blogger.client/src/components/CreateComment.vue
@@ -1,5 +1,66 @@
 <template>
   <div class="create-comment">
+    <button class="btn btn-effects"
+            data-toggle="modal"
+            data-target="#replyModal"
+    >
+      <i class="fas fa-scroll"></i>
+      Reply
+    </button>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="replyModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="replyModalLong"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <form class="form-group" @submit.prevent="createBlog">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLongTitle">
+                Reply
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <!-- <label for=""></label> -->
+              <textarea
+                class="form-control rounded-0 mt-3"
+                rows="10"
+                name=""
+                id=""
+                aria-describedby="Body"
+                placeholder="Content goes here..."
+                v-model="state.newComment.content"
+              />
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="submit" class="btn btn-primary">
+                Post
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -7,6 +68,7 @@
 export default {
   name: 'CreateComment',
   setup() {
+    state
     return {}
   },
   components: {}
@@ -14,5 +76,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.btn-effects{
+  background-color: rgb(255, 255, 255);
 
+}
+
+.btn-effects:hover{
+  background-color: rgb(146, 146, 146);
+}
 </style>
