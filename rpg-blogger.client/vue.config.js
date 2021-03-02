776302918a90111c26564f90741ed6426c49816f
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-01 23:24:39 -0700

    changed output dir

--- rpg-blogger.client/vue.config.js
+++ rpg-blogger.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: 'docs',
+  outputDir: '../rpg-blogger.server/client',
   productionSourceMap: false
 }
